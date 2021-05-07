d253d3b0f363c9e320fcfc4d0034237b6b6d120a
tamaki annen <tmn.agenkir.luck@icloud.com>
2021-05-08 00:22:06 +0900

    Initial Commit

---
+++ CaluculatorTests/CaluculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  CaluculatorTests.swift
+//  CaluculatorTests
+//
+//  Created by 安念　玉希 on 2021/05/08.
+//  Copyright © 2021 安念　玉希. All rights reserved.
+//
+
+import XCTest
+@testable import Caluculator
+
+class CaluculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
